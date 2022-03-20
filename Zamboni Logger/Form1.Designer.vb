f3908602d41a0a053e9db305b7698d5418b170e8
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-03-19 23:06:44 -0400

    More things

--- Zamboni Logger/Form1.Designer.vb
+++ Zamboni Logger/Form1.Designer.vb
@@ -35,19 +35,22 @@ Partial Class Form1
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.HeavyFlood = New System.Windows.Forms.RadioButton()
+        Me.MediumFlood = New System.Windows.Forms.RadioButton()
+        Me.LightFlood = New System.Windows.Forms.RadioButton()
         Me.GroupBox4 = New System.Windows.Forms.GroupBox()
-        Me.RadioButton7 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.HeavyCut = New System.Windows.Forms.RadioButton()
+        Me.MediumCut = New System.Windows.Forms.RadioButton()
+        Me.LightCut = New System.Windows.Forms.RadioButton()
+        Me.Logs = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.MenuStrip1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
         Me.GroupBox4.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -56,7 +59,7 @@ Partial Class Form1
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ApplicationToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(506, 28)
+        Me.MenuStrip1.Size = New System.Drawing.Size(503, 28)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -81,20 +84,21 @@ Partial Class Form1
         '
         'PictureBox1
         '
+        Me.PictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(128, 41)
+        Me.PictureBox1.Location = New System.Drawing.Point(262, 43)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(218, 113)
+        Me.PictureBox1.Size = New System.Drawing.Size(213, 113)
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(379, 334)
+        Me.Button1.Location = New System.Drawing.Point(12, 334)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(106, 29)
+        Me.Button1.Size = New System.Drawing.Size(482, 29)
         Me.Button1.TabIndex = 2
-        Me.Button1.Text = "New Ice Cut"
+        Me.Button1.Text = "Submit Ice Cut"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'GroupBox1
@@ -151,9 +155,9 @@ Partial Class Form1
         '
         'GroupBox3
         '
-        Me.GroupBox3.Controls.Add(Me.RadioButton4)
-        Me.GroupBox3.Controls.Add(Me.RadioButton3)
-        Me.GroupBox3.Controls.Add(Me.RadioButton1)
+        Me.GroupBox3.Controls.Add(Me.HeavyFlood)
+        Me.GroupBox3.Controls.Add(Me.MediumFlood)
+        Me.GroupBox3.Controls.Add(Me.LightFlood)
         Me.GroupBox3.Location = New System.Drawing.Point(256, 193)
         Me.GroupBox3.Name = "GroupBox3"
         Me.GroupBox3.Size = New System.Drawing.Size(105, 114)
@@ -161,44 +165,44 @@ Partial Class Form1
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Flood Type"
         '
-        'RadioButton4
+        'HeavyFlood
         '
-        Me.RadioButton4.AutoSize = True
-        Me.RadioButton4.Location = New System.Drawing.Point(6, 83)
-        Me.RadioButton4.Name = "RadioButton4"
-        Me.RadioButton4.Size = New System.Drawing.Size(71, 24)
-        Me.RadioButton4.TabIndex = 2
-        Me.RadioButton4.TabStop = True
-        Me.RadioButton4.Text = "Heavy"
-        Me.RadioButton4.UseVisualStyleBackColor = True
+        Me.HeavyFlood.AutoSize = True
+        Me.HeavyFlood.Location = New System.Drawing.Point(6, 83)
+        Me.HeavyFlood.Name = "HeavyFlood"
+        Me.HeavyFlood.Size = New System.Drawing.Size(71, 24)
+        Me.HeavyFlood.TabIndex = 2
+        Me.HeavyFlood.TabStop = True
+        Me.HeavyFlood.Text = "Heavy"
+        Me.HeavyFlood.UseVisualStyleBackColor = True
         '
-        'RadioButton3
+        'MediumFlood
         '
-        Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(6, 53)
-        Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(85, 24)
-        Me.RadioButton3.TabIndex = 1
-        Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Text = "Medium"
-        Me.RadioButton3.UseVisualStyleBackColor = True
+        Me.MediumFlood.AutoSize = True
+        Me.MediumFlood.Location = New System.Drawing.Point(6, 53)
+        Me.MediumFlood.Name = "MediumFlood"
+        Me.MediumFlood.Size = New System.Drawing.Size(85, 24)
+        Me.MediumFlood.TabIndex = 1
+        Me.MediumFlood.TabStop = True
+        Me.MediumFlood.Text = "Medium"
+        Me.MediumFlood.UseVisualStyleBackColor = True
         '
-        'RadioButton1
+        'LightFlood
         '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(6, 23)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(63, 24)
-        Me.RadioButton1.TabIndex = 0
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "Light"
-        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.LightFlood.AutoSize = True
+        Me.LightFlood.Location = New System.Drawing.Point(6, 23)
+        Me.LightFlood.Name = "LightFlood"
+        Me.LightFlood.Size = New System.Drawing.Size(63, 24)
+        Me.LightFlood.TabIndex = 0
+        Me.LightFlood.TabStop = True
+        Me.LightFlood.Text = "Light"
+        Me.LightFlood.UseVisualStyleBackColor = True
         '
         'GroupBox4
         '
-        Me.GroupBox4.Controls.Add(Me.RadioButton7)
-        Me.GroupBox4.Controls.Add(Me.RadioButton6)
-        Me.GroupBox4.Controls.Add(Me.RadioButton5)
+        Me.GroupBox4.Controls.Add(Me.HeavyCut)
+        Me.GroupBox4.Controls.Add(Me.MediumCut)
+        Me.GroupBox4.Controls.Add(Me.LightCut)
         Me.GroupBox4.Location = New System.Drawing.Point(379, 193)
         Me.GroupBox4.Name = "GroupBox4"
         Me.GroupBox4.Size = New System.Drawing.Size(115, 114)
@@ -206,44 +210,64 @@ Partial Class Form1
         Me.GroupBox4.TabStop = False
         Me.GroupBox4.Text = "Cut Type"
         '
-        'RadioButton7
+        'HeavyCut
         '
-        Me.RadioButton7.AutoSize = True
-        Me.RadioButton7.Location = New System.Drawing.Point(6, 82)
-        Me.RadioButton7.Name = "RadioButton7"
-        Me.RadioButton7.Size = New System.Drawing.Size(71, 24)
-        Me.RadioButton7.TabIndex = 2
-        Me.RadioButton7.TabStop = True
-        Me.RadioButton7.Text = "Heavy"
-        Me.RadioButton7.UseVisualStyleBackColor = True
+        Me.HeavyCut.AutoSize = True
+        Me.HeavyCut.Location = New System.Drawing.Point(6, 82)
+        Me.HeavyCut.Name = "HeavyCut"
+        Me.HeavyCut.Size = New System.Drawing.Size(71, 24)
+        Me.HeavyCut.TabIndex = 2
+        Me.HeavyCut.TabStop = True
+        Me.HeavyCut.Text = "Heavy"
+        Me.HeavyCut.UseVisualStyleBackColor = True
         '
-        'RadioButton6
+        'MediumCut
         '
-        Me.RadioButton6.AutoSize = True
-        Me.RadioButton6.Location = New System.Drawing.Point(6, 52)
-        Me.RadioButton6.Name = "RadioButton6"
-        Me.RadioButton6.Size = New System.Drawing.Size(85, 24)
-        Me.RadioButton6.TabIndex = 1
-        Me.RadioButton6.TabStop = True
-        Me.RadioButton6.Text = "Medium"
-        Me.RadioButton6.UseVisualStyleBackColor = True
+        Me.MediumCut.AutoSize = True
+        Me.MediumCut.Location = New System.Drawing.Point(6, 52)
+        Me.MediumCut.Name = "MediumCut"
+        Me.MediumCut.Size = New System.Drawing.Size(85, 24)
+        Me.MediumCut.TabIndex = 1
+        Me.MediumCut.TabStop = True
+        Me.MediumCut.Text = "Medium"
+        Me.MediumCut.UseVisualStyleBackColor = True
         '
-        'RadioButton5
+        'LightCut
         '
-        Me.RadioButton5.AutoSize = True
-        Me.RadioButton5.Location = New System.Drawing.Point(6, 23)
-        Me.RadioButton5.Name = "RadioButton5"
-        Me.RadioButton5.Size = New System.Drawing.Size(63, 24)
-        Me.RadioButton5.TabIndex = 0
-        Me.RadioButton5.TabStop = True
-        Me.RadioButton5.Text = "Light"
-        Me.RadioButton5.UseVisualStyleBackColor = True
+        Me.LightCut.AutoSize = True
+        Me.LightCut.Location = New System.Drawing.Point(6, 23)
+        Me.LightCut.Name = "LightCut"
+        Me.LightCut.Size = New System.Drawing.Size(63, 24)
+        Me.LightCut.TabIndex = 0
+        Me.LightCut.TabStop = True
+        Me.LightCut.Text = "Light"
+        Me.LightCut.UseVisualStyleBackColor = True
+        '
+        'Logs
+        '
+        Me.Logs.Location = New System.Drawing.Point(12, 299)
+        Me.Logs.Name = "Logs"
+        Me.Logs.Size = New System.Drawing.Size(223, 29)
+        Me.Logs.TabIndex = 20
+        Me.Logs.Text = "View Logs"
+        Me.Logs.UseVisualStyleBackColor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(14, 43)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(227, 113)
+        Me.PictureBox2.TabIndex = 21
+        Me.PictureBox2.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(506, 387)
+        Me.ClientSize = New System.Drawing.Size(503, 387)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Logs)
         Me.Controls.Add(Me.GroupBox4)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
@@ -253,7 +277,7 @@ Partial Class Form1
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Zamboni Logger"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -264,6 +288,7 @@ Partial Class Form1
         Me.GroupBox3.PerformLayout()
         Me.GroupBox4.ResumeLayout(False)
         Me.GroupBox4.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -282,10 +307,12 @@ Partial Class Form1
     Friend WithEvents BlackBear As RadioButton
     Friend WithEvents GroupBox3 As GroupBox
     Friend WithEvents GroupBox4 As GroupBox
-    Friend WithEvents RadioButton7 As RadioButton
-    Friend WithEvents RadioButton6 As RadioButton
-    Friend WithEvents RadioButton5 As RadioButton
-    Friend WithEvents RadioButton4 As RadioButton
-    Friend WithEvents RadioButton3 As RadioButton
-    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents HeavyCut As RadioButton
+    Friend WithEvents MediumCut As RadioButton
+    Friend WithEvents LightCut As RadioButton
+    Friend WithEvents HeavyFlood As RadioButton
+    Friend WithEvents MediumFlood As RadioButton
+    Friend WithEvents LightFlood As RadioButton
+    Friend WithEvents Logs As Button
+    Friend WithEvents PictureBox2 As PictureBox
 End Class
--- Zamboni Logger/Form1.vb
+++ Zamboni Logger/Form1.vb
@@ -12,6 +12,35 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BlackBear.Checked Then
+
+        ElseIf Me.Phantoms.Checked Then
+
+        Else
+
+        End If
+
+        If Me.HeavyFlood.Checked Then
+
+        ElseIf Me.MediumFlood.Checked Then
+
+        ElseIf Me.LightFlood.Checked Then
+
+        End If
+
+
+        If Me.HeavyCut.Checked Then
+
+        ElseIf Me.MediumCut.Checked Then
+
+        ElseIf Me.LightCut.Checked Then
+
+        End If
+
+
+
+
+
 
     End Sub
 
@@ -40,6 +69,18 @@
     End Sub
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles LightFlood.CheckedChanged
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs)
 
     End Sub
 End Class
